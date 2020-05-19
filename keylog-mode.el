f9e913bf119a5ca3b72f17edce909be6a11d9593
Michael Piotrowski <mxp@dynalabs.de>
2020-05-19 14:12:17 +0200

    Structure comments, move todos to README.

--- keylog-mode.el
+++ keylog-mode.el
@@ -1,9 +1,31 @@
-;;; Log only in a particular buffer---minor mode?
-;;; Save copy of current buffer as reference for later replay.
+;;; keylog-mode.el --- Keystroke logging
 
-;;; Document length *after* event is only available in post-command-hook
-;;; What needs to be logged to generate S-notation?
-;;; What does InputLog's XML look like?
+;; Copyright (c) 2020 Michael Piotrowski
+
+;; Author:    Michael Piotrowski <mxp@dynalabs.de>
+;; Created:   2020-05-17
+;; Version:   0.1
+;; Keywords:  keystroke logging, writing research
+;; Stability: unstable
+
+;; This file is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; either version 3, or (at your option)
+;; any later version.
+
+;; This file is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; A minor mode for logging keystrokes for replay and analysis.
+
+;;; Code:
 
 (defvar keylog-event-id 0 "Counter for `log-keys'.")
 
